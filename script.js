e14c282429d565d4ee0096d85e7d780d5b203ecd
FullThrottle53 <j.walston53@gmail.com>
2025-05-20 05:25:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ fetch('tickers.json')
     })
     .then(data => {
         document.getElementById('tickerDisplay').innerHTML = data.map(ticker => 
-            `<li><a href="stats/${ticker}.html">${ticker}</a></li>`
+           `<li><a href="/Test-View/stats/${ticker}.html">${ticker}</a></li>`
+
         ).join('');
     })
     .catch(error => console.error("Fetch Error:", error));  // Notice the dot before `catch`
